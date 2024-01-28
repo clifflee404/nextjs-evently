3f876fde45db7c412edfc0da39b49aecf9f33061
cliff <clifflee1022@gmail.com>
2024-01-28 18:01:57 +0800

    feat: clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next"
 import { Poppins } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs"
+
 import "./globals.css"
 
 const poppins = Poppins({
@@ -12,8 +14,8 @@ export const metadata: Metadata = {
   title: "Evently",
   description: "A platform for event management",
   icons: {
-    icon: '/assets/images/logo.svg'
-  }
+    icon: "/assets/images/logo.svg",
+  },
 }
 
 export default function RootLayout({
@@ -22,8 +24,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={poppins.variable}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
