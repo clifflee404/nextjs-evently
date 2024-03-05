b205949af0540c89392a8955f9123e956879cf38
cliff <clifflee1022@gmail.com>
2024-03-05 22:38:17 +0800

    feat: profile page myTickets/getOrdersByuser and Events Organized

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,6 +1,7 @@
 import Collection from "@/components/shared/Collection"
 import { Button } from "@/components/ui/button"
 import { getEventsByUser } from "@/lib/actions/event.actions"
+import { getOrdersByUser } from "@/lib/actions/order.actions"
 // import { getOrdersByUser } from '@/lib/actions/order.actions'
 import { IOrder } from "@/lib/database/models/order.model"
 import { SearchParamProps } from "@/types"
@@ -12,8 +13,15 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth()
   const userId = sessionClaims?.userId as string
 
+  const ordersPage = Number(searchParams?.ordersPage) || 1
   const eventsPage = Number(searchParams?.eventsPage) || 1
 
+  const orders = await getOrdersByUser({
+    userId,
+    page: ordersPage
+  })
+
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || []
   const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
 
   return (
@@ -28,7 +36,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
         </div>
       </section>
 
-      {/* <section className="wrapper my-8">
+      <section className="wrapper my-8">
         <Collection 
           data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
@@ -39,7 +47,7 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
           urlParamName="ordersPage"
           totalPages={orders?.totalPages}
         />
-      </section> */}
+      </section>
 
       {/* Events Organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
